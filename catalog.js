74edd7e67de80d17d749cc6fcbcef7c56fd445cb
DgXx25 <122464702+David3dArt@users.noreply.github.com>
2025-10-22 01:18:30 +0400

    Update catalog.js

--- catalog.js
+++ catalog.js
@@ -1,22 +1,8 @@
 window.QUIZ_CATALOG = [
   {
     title: "📘 Basic Math Quiz",
-    link: "https://raw.githubusercontent.com/David3dArt/UGSTUFF/refs/heads/main/quizzes/basic_math.csv",
-    tags: ["math", "numbers", "algebra", "mathematics", "რიცხვები", "მათემატიკა"]
+    link: "https://raw.githubusercontent.com/David3dArt/UGSTUFF/refs/heads/main/Catalogg/UG-Biology.csv",
+    tags: ["biology", "ბიოლოგია", "ადამიანის ბიოლოგია და ჯანმრთელობა",]
   },
-  {
-    title: "💡 Economics Fundamentals",
-    link: "https://raw.githubusercontent.com/David3dArt/UGSTUFF/refs/heads/main/quizzes/economics.csv",
-    tags: ["economy", "finance", "money", "ეკონომიკა", "ფინანსები"]
-  },
-  {
-    title: "🧠 Logic Thinking Challenge",
-    link: "https://raw.githubusercontent.com/David3dArt/UGSTUFF/refs/heads/main/quizzes/logic.csv",
-    tags: ["logic", "reasoning", "brain", "ლოგიკა", "აზროვნება"]
-  },
-  {
-    title: "🌍 World Geography",
-    link: "https://raw.githubusercontent.com/David3dArt/UGSTUFF/refs/heads/main/quizzes/geography.csv",
-    tags: ["geography", "countries", "world", "maps", "გეოგრაფია", "ქვეყნები"]
-  }
+
 ];
